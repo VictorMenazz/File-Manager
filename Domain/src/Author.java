ed139a84fab601ff4f6d8fc93c5e95436327d357
jordi.soley <jordi.soley@estudiantat.upc.edu>
2022-10-28 23:32:56 +0200

    Update Author attributes and add getName() getDocuments() function

--- Domain/src/Author.java
+++ Domain/src/Author.java
@@ -1,2 +1,14 @@
 public class Author {
+    String name;
+    ArrayList<Document> docs;
+
+    // Return the name of the Author
+    String getName() {
+        return name;
+    }
+
+    // Return a ArrayList of Author's Documents
+    String getDocuments() {
+        return docs;
+    }
 }
