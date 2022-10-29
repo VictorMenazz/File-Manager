ff0febb3469e8f67971202c320763bfff830ad15
jordi.soley <jordi.soley@estudiantat.upc.edu>
2022-10-29 22:04:52 +0200

    Update functions

--- Domain/src/Author.java
+++ Domain/src/Author.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Author {
     String name;
     ArrayList<Document> docs;
--- Domain/src/SetAuthors.java
+++ Domain/src/SetAuthors.java
@@ -1,2 +1,20 @@
+import java.util.HashMap;
+
 public class SetAuthors {
+    HashMap<String, Author> authors;
+
+    // Add a new Author
+    void addAuthor(String name, Author a) {
+        authors.put(name, a);
+    }
+
+    // Remove a Author
+    void delAuthor(String name) {
+        authors.remove(name);
+    }
+
+    // Returns the total number of authors
+    int size() {
+        return authors.size();
+    }
 }
