cc79031d7083b3d0879227a9b4c93d1ec2cbf71a
jordi.soley <jordi.soley@estudiantat.upc.edu>
2022-11-03 15:47:11 +0100

    Add matchesPrefix(prefix) function added to Authors class

--- Code/src/Domain/Classes/Author.java
+++ Code/src/Domain/Classes/Author.java
@@ -2,17 +2,47 @@ package Code.src.Domain.Classes;
 
 import java.util.ArrayList;
 
+/**
+ * @brief Class Author that contains the information of a Author
+ *
+ * @author Jordi Soley Masats
+ */
 public class Author {
     String name;
     ArrayList<Document> docs;
 
-    // Return the name of the Author
-    String getName() {
+    /**
+     * @brief Return the name of the Author
+     *
+     * @return
+     */
+    public String getName() {
         return name;
     }
 
-    // Return a ArrayList of Author's Documents
+    /**
+     * @brief Return a ArrayList of Author's Documents
+     *
+     * @return
+     */
     ArrayList<Document> getDocuments() {
         return docs;
     }
+
+    /**
+     * @brief Checks if the author's name starts with a given prefix
+     *
+     * @param prefix
+     *
+     * @return true if yes, false if not
+     */
+    boolean matchesPrefix(String prefix) {
+        int size = prefix.length();
+        if (size > name.length()) return false;
+        for (int i = 0; i < size; ++i) {
+            if (prefix.charAt(i) != name.charAt(i)) return false;
+        }
+        return true;
+    }
+
 }
