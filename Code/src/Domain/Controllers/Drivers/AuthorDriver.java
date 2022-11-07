1e0526b7884ab3ad9c4189eeba6b51e84abaad95
Vikoey04 <vikoey04@gmail,com>
2022-11-07 12:52:06 +0100

    Add AuthorDriver and SetAuthorsDriver

---
+++ Code/src/Domain/Controllers/Drivers/AuthorDriver.java
@@ -0,0 +1,24 @@
+package Code.src.Domain.Controllers.Drivers;
+
+import Code.src.Domain.Classes.Document;
+import org.junit.*;
+
+import java.util.ArrayList;
+
+public class AuthorDriver {
+
+    @Test
+    public static String TestGetName() {
+
+    }
+
+    @Test
+    public static ArrayList<Document> TestGetDocuments() {
+
+    }
+
+    @Test
+    public static boolean TestMatchesPrefix() {
+
+    }
+}
---
+++ Code/src/Domain/Controllers/Drivers/SetAuthorsDriver.java
@@ -0,0 +1,28 @@
+package Code.src.Domain.Controllers.Drivers;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+public class SetAuthorsDriver {
+
+    @Test
+    public static void TestAddAuthor() {
+
+    }
+
+    @Test
+    public static void TestDelAuthor {
+
+    }
+
+    @Test
+    public static int TestSize {
+
+    }
+
+    @Test
+    public static ArrayList<String> TestNamesAuthorsMatch() {
+
+    }
+}
