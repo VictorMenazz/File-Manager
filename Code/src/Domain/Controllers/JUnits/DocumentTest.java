c4b8c16bdaab7f13886976dd538a4f46459c1b98
Marc Navarro <marc.navarro.acosta@estudiantat.upc.edu>
2022-11-07 21:53:11 +0100

    Adding /lib and initializing Document and Folder JUnit.

---
+++ Code/src/Domain/Controllers/JUnits/DocumentTest.java
@@ -0,0 +1,50 @@
+package Code.src.Domain.Controllers.JUnits;
+
+import static org.junit.Assert.*;
+
+public class DocumentTest {
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+    }
+
+    @org.junit.Test
+    public void setAuthor() {
+    }
+
+    @org.junit.Test
+    public void setTitle() {
+    }
+
+    @org.junit.Test
+    public void setContent() {
+    }
+
+    @org.junit.Test
+    public void setLanguage() {
+    }
+
+    @org.junit.Test
+    public void protectDocument() {
+    }
+
+    @org.junit.Test
+    public void getTitle() {
+    }
+
+    @org.junit.Test
+    public void getAuthor() {
+    }
+
+    @org.junit.Test
+    public void getContent() {
+    }
+
+    @org.junit.Test
+    public void getLanguage() {
+    }
+}
