2bbb3f50ba07764cb42bfbe7aae584e03c217b5e
Juliia-ad <julia.amenos3@gmail.com>
2022-12-23 03:38:57 +0100

    Update

--- FONTS/src/Interface/DocumentSearch.java
+++ FONTS/src/Interface/DocumentSearch.java
@@ -50,10 +50,9 @@ public class DocumentSearch extends JPanel {
         modelA = new DefaultComboBoxModel<>(array);
         authors = new JComboBox<>(modelA);
 
-        if (authors.getSelectedIndex() >= 0) {
-            String a = (String) authors.getSelectedItem();
-            loadTitles(a);
-        }
+        String a = (String) authors.getSelectedItem();
+        loadTitles(a);
+
 
         c.gridx = 0;
         c.gridy = 0;
@@ -61,19 +60,19 @@ public class DocumentSearch extends JPanel {
         add(titleView, c);
         c.gridwidth = 1;
         c.gridx = 0;
-        c.gridy = 2;
+        c.gridy = 1;
         add(l, c);
         c.gridx = 1;
-        c.gridy = 2;
+        c.gridy = 1;
         add(authors, c);
         c.gridx = 0;
+        c.gridy = 2;
+        add(l2, c);
+        c.gridx = 1;
+        c.gridy = 2;
+        add(titles, c);
+        c.gridx = 0;
         c.gridy = 3;
-        add(l2);
-        if (authors.getSelectedIndex() >= 0) {
-            c.gridx = 1;
-            c.gridy = 3;
-            add(titles);
-        }
         c.gridwidth = 2;
         add(search, c);
 
@@ -89,79 +88,28 @@ public class DocumentSearch extends JPanel {
         setPreferredSize(new Dimension(500,350));
         setMaximumSize(new Dimension(500,350));
         setMinimumSize(new Dimension(500,350));
-        titleView.setBounds(175,5,200,30);
-        add(titleView);
 
-        //Obtener lista dsd controlador;
-        ArrayList<String> list = CtrlPres.getAuthorsName();
-        authors = new JComboBox(list.toArray());
-        authors.setBounds(160, 100, 200, 20);
-        add(authors);
-
-        if (!list.isEmpty()){
-            ArrayList<String> t = CtrlPres.getAuthorDocuments(list.get(authors.getSelectedIndex()));
-            titles = new JComboBox(t.toArray());
-            titles.setBounds(160, 150, 200, 20);
-            add(titles);
-        }
-
-
-        l.setBounds(100,100,200,20);
-        add(l);
-        l2.setBounds(100, 150, 200, 20);
-        add(l2);
-        search.setBounds(200,200,100,20);
-        add(search);
-
+        load();
         setVisible(true);
 
 
 
-        /*ActionListener SearchTitles = new ActionListener() {
+        ActionListener SearchDocument = new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String titulos = CtrlPres.SearchTitles(txtAutor.getText());
-                if (titulos == null) {
-                    ferror();
+                if (e.getSource() == search){
+                    String a = (String) authors.getSelectedItem();
+                    String t = (String) titles.getSelectedItem();
+                    CtrlPres.toDocument(a, t, "null", false, true);
                 }
-                else {
-                    JPanel middlePanel = new JPanel();
-                    JButton BOkay = new JButton("Aceptar");
-                    middlePanel.setBorder(new TitledBorder(new EtchedBorder(), "Titulos del autor " + txtAutor.getText()));
-
-                    JTextArea display = new JTextArea(20, 22);
-                    display.setText(titulos);
-                    display.setEditable(false);
-                    JScrollPane scroll = new JScrollPane(display);
-                    scroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-
-                    middlePanel.add(scroll);
-
-                    JFrame frame = new JFrame();
-                    frame.add(middlePanel);
-                    frame.pack();
-                    frame.setLocationRelativeTo(null);
-                    frame.setVisible(true);
-                    frame.setResizable(false);
-
-
-                }
-                ActionListener Salir = new ActionListener() {
-                    @Override
-                    public void actionPerformed(ActionEvent e) {
-                        txtAutor.setText("");
-                    }
-                };
-                //BOkay.addActionListener(Salir);
-
             }
-        };*/
-        //search.addActionListener(BuscarTitulos);
+        };
+        search.addActionListener(SearchDocument);
     }
 
     public void reset() {
-        if(authors != null)authors.setSelectedIndex(-1);
-        if(titles != null) titles.setSelectedIndex(-1);
+        if (authors != null) authors.setSelectedIndex(-1);
+        if (titles != null) titles.setSelectedIndex(-1);
         load();
         setVisible(true);
     }
