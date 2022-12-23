3e163526d2e5775f6eb2cdd596a989856f784fbb
victor.mena <victor.mena@estudiantat.upc.edu>
2022-12-23 13:09:07 +0100

    fixed results

--- FONTS/src/Interface/RelevantDocumentsSearch.java
+++ FONTS/src/Interface/RelevantDocumentsSearch.java
@@ -1,6 +1,7 @@
 package FONTS.src.Interface;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.text.NumberFormatter;
 import java.awt.*;
@@ -138,17 +139,24 @@ public class RelevantDocumentsSearch extends JPanel {
     public void showResults(HashMap<String, String> result) {
         removeAll();
 
-        Object[][] data = new Object[result.size()][3];
+        setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+        c.insets = new Insets(10, 0, 10, 0);
+
+        JLabel label = new JLabel("Results relevant documents:");
+
+        Object[][] data = new Object[result.size()][4];
         Icon documentIcon = new ImageIcon(new ImageIcon("FONTS/src/Interface/Utils/icone-fichier-document-noir.png").getImage().getScaledInstance(20, 20, Image.SCALE_DEFAULT));
         int j = 0;
         for (String key : result.keySet()) {
-            data[j][0] = documentIcon;
-            data[j][1] = result.get(key);
-            data[j][2] = key;
+            data[j][0] = j+1;
+            data[j][1] = documentIcon;
+            data[j][2] = result.get(key);
+            data[j][3] = key;
             ++j;
         }
 
-        String[] columnNames = {"Type", "Name", "Author"};
+        String[] columnNames = {"Relevance", "Type", "Name", "Author"};
         model = new DefaultTableModel(data, columnNames) {
             //  Returning the Class of each column will allow different
             //  renderers to be used based on Class
@@ -168,13 +176,22 @@ public class RelevantDocumentsSearch extends JPanel {
         table.setAutoCreateRowSorter(true);
         table.setShowVerticalLines(false);
         table.setRowHeight(30);
-        table.getColumnModel().getColumn(0).setMinWidth(50);
-        table.getColumnModel().getColumn(0).setMaxWidth(50);
+        table.getColumnModel().getColumn(0).setMinWidth(80);
+        table.getColumnModel().getColumn(0).setMaxWidth(80);
+        DefaultTableCellRenderer leftRenderer = new DefaultTableCellRenderer();
+        leftRenderer.setHorizontalAlignment( JLabel.LEFT );
+        table.getColumnModel().getColumn(0).setCellRenderer(leftRenderer);
+        table.getColumnModel().getColumn(1).setMinWidth(50);
+        table.getColumnModel().getColumn(1).setMaxWidth(50);
 
         JScrollPane tableScroll = new JScrollPane(table);
         tableScroll.setPreferredSize(new Dimension(500, 350));
 
-        add(tableScroll);
+        c.gridx = 0;
+        c.gridy = 0;
+        add(label, c);
+        c.gridy = 1;
+        add(tableScroll, c);
 
         updateUI();
         setVisible(true);
